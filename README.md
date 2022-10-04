00b955f80b894bee0d5b29b8172064284c195705
Derrick Ray <dray@drayconcepts.com>
2022-10-04 13:05:05 -0500

    Course completed

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+## Tutorial
+
+[Next.js Crash Course - Traversy Media](https://youtu.be/mTz0GXj8NN0)
+
 ## Getting Started
 
 First, run the development server:
---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className={headerStyles.title}>
+            <span>WebDev</span> Newz
+        </h1>
+        <p className={headerStyles.description}>Keep up to date with the latest web dev newz.</p>
+    </div>
+  )
+}
+
+export default Header
